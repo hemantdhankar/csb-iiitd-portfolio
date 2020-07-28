9e83f944ad8a93539d6965c394f7118ca62406d3
TheWizard25 <hemantdhankar123@gmail.com>
2020-07-29 01:55:20 +0530

    Styling for projects done

--- csbiiitdportfolio-project/projects/views.py
+++ csbiiitdportfolio-project/projects/views.py
@@ -5,5 +5,5 @@ from .models import Project
 # Create your views here.
 
 def home(request):
-	projects = Project.objects
+	projects = Project.objects.order_by('projectName')
 	return render(request, 'index.html', {'projects':projects})
