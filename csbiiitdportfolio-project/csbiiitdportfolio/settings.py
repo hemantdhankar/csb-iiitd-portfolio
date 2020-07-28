c2bdec365047a08188bb225cde4f4acbf96d9be8
samarth1107 <samarth1107@gmail.com>
2020-07-28 13:49:26 +0530

    Template changes

--- csbiiitdportfolio-project/csbiiitdportfolio/settings.py
+++ csbiiitdportfolio-project/csbiiitdportfolio/settings.py
@@ -20,11 +20,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '-t^a$utunj9(f!^7e)-@lhdl$0$u-c$yq4mrkk6r1no1!i)x$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Application definition
