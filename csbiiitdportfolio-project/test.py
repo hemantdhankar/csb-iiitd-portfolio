fcc231d7874c7fd4385c41cb106dcde16bdc24f3
samarth1107 <samarth1107@gmail.com>
2020-08-16 19:08:35 +0530

    Image transformation to 1:1

--- csbiiitdportfolio-project/test.py
+++ csbiiitdportfolio-project/test.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from projects.models import Project
 
-path = "E:\Project\Portfolio\CSB Portfolio (Responses).csv"
+path = "E:\Project\Portfolio\Updated_response.csv"
 Main_df = pd.read_csv(path)
 
 for index in Main_df.index:
     category = Main_df.loc[index,"Type of project"].split(",")
-    Project.objects.create(projectName=Main_df.loc[index,"Title"], projectDescription=Main_df.loc[index,"Description"], projectCategory=category, projectTeam=Main_df.loc[index,"Team Member (use comma ',' for separator, if individual project write only your name)"], projectLink=Main_df.loc[index,"Link(if hosted like website, play store link etc) "])
+    Project.objects.create(projectName=Main_df.loc[index,"Project Title (max 50 character)"], projectDescription=Main_df.loc[index,"Description (max 30 words, 200 characters)"], projectCategory=category, projectTeam=Main_df.loc[index,"Team Member (use comma ',' for separator, if individual project write only your name)"], projectLink=Main_df.loc[index,"Web Link(if hosted like a website) The website can be your working project or a site having a description of your project."])
